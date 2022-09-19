e62e2039510489f54c45694667fac30a24c3d2fa
Manuel_Buesaquillo <manuel.gera@hotmail.com>
2022-09-19 00:02:51 -0500

    2

---
+++ js/api.js
@@ -0,0 +1,74 @@
+var resultado = null;
+$(document).ready(function() {
+    $.ajax({
+        url: 'https://thronesapi.com/api/v2/Characters',
+        success: function(res) {
+            resultado = res;
+            /* console.log('data :>> ', res); */
+            let cards = '';
+            res.forEach(usr => {
+                const card = `
+                    <div class="card">
+                        <div class="img-card">
+                            <img src="${usr.imageUrl}" alt="${usr.firstName}" class="img">
+                        </div>
+                        <div class="info-card">
+                            <h5>${usr.fullName}</h5>
+                        </div>
+                    </div>`;
+                cards += card;
+            });
+            $('.reparto').html(cards);
+        },
+        error: function(err) {
+            console.log('err :>> ', err);
+        }
+    })
+    .done(function() {
+        console.log('Finaliza ejecucion del ajax');
+    });
+    $.ajax({
+        url: 'https://api.got.show/api/show/episodes',
+        success: function(res) {
+            resultado = res;
+            console.log('data :>> ', res);
+            let cards = '<h4 align="center">EPISODIOS</h4>';
+            res.forEach(usr => {
+                const card = `
+                    <div class="card2">
+                        <div>
+                            <h4>Titulo: ${usr.title}</h4>
+                            <h5>Temporada: ${usr.season}</h5>
+                            <h5>Episodio: ${usr.episode}</h5>
+                        </div>
+                    </div>`;
+                cards += card;
+            });
+            $('.info').html(cards);
+        },
+        error: function(err) {
+            console.log('err :>> ', err);
+        }
+    })
+    .done(function() {
+        console.log('Finaliza ejecucion del ajax');
+    });
+
+    $('.form').validate({
+        rules: {
+            name: "campos",
+            nombres: {
+                required: true,
+                minlength: 6,
+                maxlength: 50
+            }
+        },
+        messages: {
+            nombres: {
+                required: "El campo nombre es requerido.",
+                minlength: "El tamaño minimo son 6 caracteres",
+                maxlength: "El tamaño max son 50 caracteres."
+            }
+        }
+    });
+});
---
+++ js/app.js
@@ -0,0 +1,38 @@
+const socket = new WebSocket('wss://ws.bitmex.com/realtime');
+
+socket.onopen = event => {
+    const apiCall = {
+        'op': 'subscribe',
+        'args': [`orderBookL2_25:XBTUSD`]
+    };
+    /* console.log('Inicia socket', event); */
+    socket.send(JSON.stringify(apiCall));
+};
+
+socket.onmessage = event => {
+    const json = JSON.parse(event.data);
+
+    if (json.action === 'partial' || json.action === 'insert') {
+        /* console.log('json.data :>> ', json.data); */
+        let precioMayor = 0;
+        let moneda = '';
+        let tieneBuy = false;
+        json.data.forEach(row => {
+            if (row.side === 'Buy') {
+                precioMayor = row.price > precioMayor ? row.price : precioMayor;
+                tieneBuy = true;
+            }
+            moneda = row.symbol;
+        });
+
+        /* console.log('mensaje recibido filtrado', precioMayor); */
+        if (tieneBuy) {
+            $('.valores-moneda').text(`USD$ ${precioMayor}`);
+            $('.label-moneda').text(`Cryptomoneda: ${moneda}`);
+        }
+    }
+};
+
+socket.onerror = event => {
+    console.log('error recibido', event);
+};
